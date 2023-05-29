b1e5aaab6c3a180b1d8be5f5d0a903b8b5c254bc
ZahraHamani <zah.hamani.ra@gmail.com>
2023-05-29 10:04:20 +0200

    ok

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -17,6 +17,7 @@ namespace Security_P1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            ////////////////////////////////////////////
         }
     }
 }
